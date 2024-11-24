a28b94ca770b632437ba17fa80a84f1c26698a0e
Akella348 <akella348@gmail.com>
2024-11-24 22:53:54 +0400

    2 iteration

--- module_13_4.py
+++ module_13_4.py
@@ -14,8 +14,6 @@ class UserState(StatesGroup):
     growth = State()
     weight = State()
 
-
-
 @dp.message_handler(commands = ['start'])
 async def start_message(message):
     await message.answer('Привет! Я бот помогающий твоему здоровью. Напишите Calories, чтобы начать.')
