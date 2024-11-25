8092c5772582f7d88ef4f426cfe9813c396dad42
Akella348 <akella348@gmail.com>
2024-11-26 01:23:03 +0400

    1 iteration

---
+++ crud_functions.py
@@ -0,0 +1,30 @@
+import sqlite3
+def initiate_db():
+
+    connection = sqlite3.connect('products.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    )
+    ''')
+    cursor.execute('SELECT COUNT(*) FROM Products')
+    if cursor.fetchone()[0] == 0:
+
+        for i in range(1,6):
+            cursor.execute("INSERT INTO Products (title, description, price) VALUES (?,?,?)",
+                           (f"Product{i}", f"описание {i}", i * 100))
+
+    connection.commit()
+    connection.close()
+def get_all_products():
+    connection = sqlite3.connect('products.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Products')
+    products = cursor.fetchall()
+    connection.close()
+    return products
